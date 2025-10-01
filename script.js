b04807c6e79eef230f2cb9414f09dc09cb925a1a
Arjit Prakher <arjitprakher4@gmail.com>
2025-10-01 12:06:23 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+function getRandomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+function getRandomNumber(max, min) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+let theUsers = [];
+function generateRandomUser(count) {
+    const firstNames = ["Arjun", "Meera", "Rahul", "Priya", "Vikram", "Sneha", "Karan", "Anjali", "Rohit", "Pooja"];
+    const lastNames = ["Sharma", "Verma", "Singh", "Patel", "Iyer", "Nair", "Das", "Gupta", "Kapoor", "Joshi"];
+    const states = ["Maharashtra", "Karnataka", "Tamil Nadu", "Gujarat", "Punjab", "Rajasthan", "Bihar", "West Bengal", "Kerala", "Uttar Pradesh"];
+    const skillsPool = ["JavaScript", "Python", "Java", "C++", "SQL", "Node.js", "React", "Angular", "Django", "Flask", "HTML", "CSS", "AWS", "Docker", "Kubernetes"];
+
+
+    // const profileImageURL = "https://picsum.photos/id/237/200/300";
+    for (let i = 0; i < count; i++) {
+        const fullname = `${getRandomItem(firstNames)} ${getRandomItem(lastNames)}`;
+        const age = getRandomNumber(18, 50);
+        const skills = [...skillsPool].sort(() => 0.5 - Math.random()).slice(0, 5);
+        const state = getRandomItem(states);
+        const isAvailable = Math.random() < 0.5;
+        const profileImageID = `https://picsum.photos/id/${getRandomNumber(9, 100)}/150/150`
+
+        theUsers.push({
+            fullname,
+            profileImageID,
+            age,
+            skills,
+            state,
+            isAvailable
+        });
+
+    }
+    return theUsers;
+}
+
+generateRandomUser(10);
+
+
+let cardContainer = document.getElementById('cards-container');
+function heroFuntion() {
+    let allCardDiv = '';
+
+    theUsers.forEach(function (elem, idx) {
+        allCardDiv = allCardDiv + `<div class="card">
+                <img src="${elem.profileImageID}" alt="">
+                <h3>${elem.fullname}, ${elem.age}</h3>
+                
+                <h4>${elem.isAvailable ? "available" : "not available"}</h4>
+                <button id=${idx}>View more</button>
+            </div>`;
+    });
+    cardContainer.innerHTML = allCardDiv;
+}
+heroFuntion();
+
+cardContainer.addEventListener('click', function (dets) {
+    let goldenValues = theUsers[dets.target.id];
+    
+})
+
+
